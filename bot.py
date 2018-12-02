6d55fa0a80103bb769f4494ac131e392f01892e7
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-02 23:49:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,12 +65,12 @@ async def help():
 async def help_nsfw():
         embed = discord.Embed(color = 0xFF00FF, title = "NSFW help")
     
-        embed.add_field(name = ":boobs", value = "Ukáže to random kozi", inline = False)
-        embed.add_field(name = ":pussy", value = "Ukáže to random vagínu (Neni hotovo)", inline = False)
-        embed.add_field(name = ":ass", value = "Ukáže to random zadek! (Neni hotovo)", inline = False)
-        embed.add_field(name = ":anal", value = "Ukáže to random gif šukání do análu (Neni hotovo)", inline = False)
+        embed.add_field(name = ":boobs", value = "Ukáže to random prsa", inline=False)
+        embed.add_field(name = ":pussy", value = "Ukáže to random vagínu (Neni hotovo)", inline=False)
+        embed.add_field(name = ":ass", value = "Ukáže to random zadek! (Neni hotovo)", inline=False)
+        embed.add_field(name = ":anal", value = "Ukáže to random gif šukání do análu (Neni hotovo)", inline=False)
         embed.set_footer(text = "Udělala Nela | v0.3")
-     await client.say(embed=embed)
+        await client.say(embed=embed)
                   
     
 
